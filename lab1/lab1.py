286ee301148d0fe5d479a011429cacdf63310034
student <student@lab-kss-2.cs.put.poznan.pl>
2025-10-11 13:44:20 +0200

    Fixed lab1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,14 +1,9 @@
 import math
 
-def h(n,
-      A=1.5,
-      beta=0.035,
-      Tp=0.1,
-      Qd=0.05
-):
-    return (1/A * (Qd * n - beta * math.sqrt(h(n))) * Tp) + h(n)
-
-
+def update_h(prev_h, A=1.5, beta=0.035, Tp=0.1, Qd=0.05):
+    # Update equation for stable h
+    dh = (Qd - beta * math.sqrt(prev_h)) * Tp / A
+    return prev_h + dh
 
 if __name__ == '__main__':
     A = 1.5 # m^2/s
@@ -18,11 +13,13 @@ if __name__ == '__main__':
     Qd = 0.05 # m^3/s
 
     h_all = []
+    h_current = 0.0  # Initial h value
+
     # Create time series
-    for i in range(t):
-        h_calculated = h(n = i*Tp)
-        print(h_calculated)
-        h_all.append(h_calculated)
+    for _ in range(int(t//Tp) + 1):
+        h_next = update_h(h_current, A, beta, Tp, Qd)
+        print(h_next)
+        h_all.append(h_next)
+        h_current = h_next  # Update for next iteration
 
     print(h_all)
-
